7354aa21f8cd32b709949062faf1229d8bcec8cb
kkc217 <kkc217@naver.com>
2022-07-29 15:39:29 +0900

    검색 기능 수정

--- src/main/java/com/yeoreodigm/server/controller/SearchApiController.java
+++ src/main/java/com/yeoreodigm/server/controller/SearchApiController.java
@@ -1,14 +1,11 @@
 package com.yeoreodigm.server.controller;
 
-import com.yeoreodigm.server.domain.Places;
 import com.yeoreodigm.server.dto.PageResult;
 import com.yeoreodigm.server.dto.SearchPlacesResponseDto;
-import com.yeoreodigm.server.dto.search.SearchRequestDto;
 import com.yeoreodigm.server.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,18 +15,18 @@ public class SearchApiController {
 
     private final PlaceService placeService;
 
-    @GetMapping("/place/{page}")
+    @GetMapping("/place/{content}/{page}")
     public PageResult<List<SearchPlacesResponseDto>> searchPlaces(
-            @RequestBody @Valid SearchRequestDto searchRequestDto,
+            @PathVariable("content") String content,
             @PathVariable("page") int page) {
         List<SearchPlacesResponseDto> responseDtoList =
                 placeService
-                        .searchPlaces(searchRequestDto.getContent(), page)
+                        .searchPlaces(content, page)
                         .stream()
                         .map(SearchPlacesResponseDto::new)
                         .toList();
 
-        int next = placeService.checkNextSearchPlaces(searchRequestDto.getContent(), page);
+        int next = placeService.checkNextSearchPlaces(content, page);
 
         return new PageResult<>(responseDtoList, next);
     }
