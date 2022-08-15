84c2bdad7af4fe77508b42bb0dc150fb8cb45bd4
kkc217 <kkc217@naver.com>
2022-08-16 02:56:10 +0900

    [#101] 여행지 좋아요 수정 API 구현

--- src/main/java/com/yeoreodigm/server/controller/PlaceDetailApiController.java
+++ src/main/java/com/yeoreodigm/server/controller/PlaceDetailApiController.java
@@ -1,14 +1,16 @@
 package com.yeoreodigm.server.controller;
 
 import com.yeoreodigm.server.domain.Member;
-import com.yeoreodigm.server.domain.Places;
 import com.yeoreodigm.server.dto.constraint.SessionConst;
 import com.yeoreodigm.server.dto.detail.place.PlaceDetailResponseDto;
+import com.yeoreodigm.server.dto.detail.place.PlaceLikeRequestDto;
 import com.yeoreodigm.server.dto.like.LikeItemDto;
 import com.yeoreodigm.server.service.PlaceLikeService;
 import com.yeoreodigm.server.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +32,13 @@ public class PlaceDetailApiController {
             @PathVariable("placeId") Long placeId,
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
         return placeLikeService.getLikeInfo(placeService.getPlaceById(placeId), member);
+    }
+
+    @PatchMapping("/like")
+    public void changePlaceLike(
+            @RequestBody @Valid PlaceLikeRequestDto requestDto,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
+        placeLikeService.changePlaceLike(member, requestDto.getPlaceId(), requestDto.isLike());
     }
 
 }
---
+++ src/main/java/com/yeoreodigm/server/dto/detail/place/PlaceLikeRequestDto.java
@@ -0,0 +1,12 @@
+package com.yeoreodigm.server.dto.detail.place;
+
+import lombok.Data;
+
+@Data
+public class PlaceLikeRequestDto {
+
+    private Long placeId;
+
+    private boolean like;
+
+}
