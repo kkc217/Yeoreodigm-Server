8f8b8388136584654320b6bed2052f804cc08fa9
kkc217 <kkc217@naver.com>
2022-08-26 16:10:00 +0900

    [#108] 여행지 좋아요 정보 조회 API 추가

--- src/main/java/com/yeoreodigm/server/controller/PlaceApiController.java
+++ src/main/java/com/yeoreodigm/server/controller/PlaceApiController.java
@@ -6,6 +6,7 @@ import com.yeoreodigm.server.domain.Places;
 import com.yeoreodigm.server.dto.PageResult;
 import com.yeoreodigm.server.dto.constraint.QueryConst;
 import com.yeoreodigm.server.dto.constraint.SessionConst;
+import com.yeoreodigm.server.dto.like.LikeItemDto;
 import com.yeoreodigm.server.dto.place.PlaceResponseDto;
 import com.yeoreodigm.server.service.PlaceLikeService;
 import com.yeoreodigm.server.service.PlaceService;
@@ -24,7 +25,7 @@ public class PlaceApiController {
     private final PlaceLikeService placeLikeService;
 
     @GetMapping("/like/list/{page}")
-    public PageResult<List<PlaceResponseDto>> callPlaceLike(
+    public PageResult<List<PlaceResponseDto>> callPlaceLikeList(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member,
             @PathVariable("page") int page) {
         List<PlaceLike> placeLikeList
@@ -37,4 +38,10 @@ public class PlaceApiController {
         return new PageResult<>(placeList.stream().map(PlaceResponseDto::new).toList(), next);
     }
 
+    @GetMapping("/like/{placeId}")
+    public LikeItemDto callPlaceLikeItem(
+            @PathVariable("placeId") Long placeId,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
+        return placeLikeService.getLikeInfo(placeService.getPlaceById(placeId), member);
+    }
 }
