18312f60f9c6e35a3d4081ba2fc9baccf3fda6f9
kkc217 <kkc217@naver.com>
2022-08-27 00:48:14 +0900

    [#111] 여행 노트 상세페이지 댓글 API 리팩토링

--- src/main/java/com/yeoreodigm/server/controller/TravelNoteDetailApiController.java
+++ src/main/java/com/yeoreodigm/server/controller/TravelNoteDetailApiController.java
@@ -3,6 +3,7 @@ package com.yeoreodigm.server.controller;
 import com.yeoreodigm.server.domain.Course;
 import com.yeoreodigm.server.domain.Member;
 import com.yeoreodigm.server.domain.TravelNote;
+import com.yeoreodigm.server.dto.ContentRequestDto;
 import com.yeoreodigm.server.dto.PageResult;
 import com.yeoreodigm.server.dto.Result;
 import com.yeoreodigm.server.dto.comment.CommentItemDto;
@@ -55,6 +56,38 @@ public class TravelNoteDetailApiController {
         return new NoteDetailInfoResponseDto(travelNoteService.getTravelNoteDetailInfo(travelNote));
     }
 
+    @GetMapping("/comment/{travelNoteId}")
+    public Result<List<CommentItemDto>> callTravelNoteComment(
+            @PathVariable("travelNoteId") Long travelNoteId,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
+        return new Result<>(noteCommentService.getNoteCommentInfo(
+                travelNoteService.getTravelNoteById(travelNoteId), member));
+    }
+
+    @PostMapping("/comment")
+    public void addTravelNoteComment(
+            @RequestBody @Valid ContentRequestDto requestDto,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
+        noteCommentService.addNoteComment(
+                member,
+                travelNoteService.getTravelNoteById(requestDto.getId()),
+                requestDto.getContent());
+    }
+
+    @DeleteMapping("/comment/{commentId}")
+    public void deleteTravelNoteComment(
+            @PathVariable(name = "commentId") Long commentId,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
+        noteCommentService.deleteNoteComment(member, commentId);
+    }
+
+    @PostMapping("/comment/like")
+    public void changeTravelNoteCommentLike(
+            @RequestBody @Valid LikeRequestDto requestDto,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
+        noteCommentLikeService.changeTravelNoteLike(member, requestDto.getId(), requestDto.isLike());
+    }
+
     @GetMapping("/like/{travelNoteId}")
     public LikeItemDto callTravelNoteLike(
             @PathVariable("travelNoteId") Long travelNoteId,
@@ -69,53 +102,6 @@ public class TravelNoteDetailApiController {
         travelNoteLikeService.changeTravelNoteLike(member, requestDto.getId(), requestDto.isLike());
     }
 
-    @GetMapping("/course/{travelNoteId}/{day}")
-    public PageResult<NoteDetailCourseResponseDto> callTravelNoteDetailCourse(
-            @PathVariable("travelNoteId") Long travelNoteId,
-            @PathVariable("day") int day) {
-        TravelNote travelNote = travelNoteService.getTravelNoteById(travelNoteId);
-
-        Course course = courseService.getCourseByTravelNoteAndDay(travelNote, day);
-        RouteInfoDto routeInfo = courseService.getRouteInfoByCourse(course);
-
-        int next = courseService.checkNextCoursePage(
-                travelNote, day, 1);
-
-        return new PageResult<>(new NoteDetailCourseResponseDto(routeInfo, placeService.getPlacesByCourse(course)), next);
-    }
-
-    @GetMapping("/comment/{travelNoteId}")
-    public Result<List<CommentItemDto>> callTravelNoteComment(
-            @PathVariable("travelNoteId") Long travelNoteId,
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
-        return new Result<>(noteCommentService.getNoteCommentInfo(
-                travelNoteService.getTravelNoteById(travelNoteId), member));
-    }
-
-    @PostMapping("/comment/add")
-    public CommentItemDto addTravelNoteComment(
-            @RequestBody @Valid NoteCommentRequestDto requestDto,
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
-        return noteCommentService.addNoteComment(
-                member,
-                travelNoteService.getTravelNoteById(requestDto.getTravelNoteId()),
-                requestDto.getText());
-    }
-
-    @PostMapping("/comment/delete")
-    public void deleteTravelNoteComment(
-            @RequestBody @Valid NoteCommentRequestDto requestDto,
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
-        noteCommentService.deleteNoteComment(member, requestDto.getCommentId());
-    }
-
-    @PostMapping("/comment/like")
-    public void changeTravelNoteCommentLike(
-            @RequestBody @Valid LikeRequestDto requestDto,
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member) {
-        noteCommentLikeService.changeTravelNoteLike(member, requestDto.getId(), requestDto.isLike());
-    }
-
     @PostMapping("/make")
     public TravelNoteIdResponseDto makeMyTravelNote(
             @RequestBody @Valid TravelNoteRequestDto requestDto,
@@ -127,5 +113,20 @@ public class TravelNoteDetailApiController {
 
         return new TravelNoteIdResponseDto(newTravelNoteId);
     }
+
+//    @GetMapping("/course/{travelNoteId}/{day}")
+//    public PageResult<NoteDetailCourseResponseDto> callTravelNoteDetailCourse(
+//            @PathVariable("travelNoteId") Long travelNoteId,
+//            @PathVariable("day") int day) {
+//        TravelNote travelNote = travelNoteService.getTravelNoteById(travelNoteId);
+//
+//        Course course = courseService.getCourseByTravelNoteAndDay(travelNote, day);
+//        RouteInfoDto routeInfo = courseService.getRouteInfoByCourse(course);
+//
+//        int next = courseService.checkNextCoursePage(
+//                travelNote, day, 1);
+//
+//        return new PageResult<>(new NoteDetailCourseResponseDto(routeInfo, placeService.getPlacesByCourse(course)), next);
+//    }
 
 }
