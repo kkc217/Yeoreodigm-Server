a02a4c513493bf7a689f05f1a9bd814fe519126d
kkc217 <kkc217@naver.com>
2022-09-20 00:22:43 +0900

    테스트 코드 추가

---
+++ src/test/java/com/yeoreodigm/server/controller/MemberApiControllerTest.java
@@ -0,0 +1,86 @@
+package com.yeoreodigm.server.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yeoreodigm.server.domain.Gender;
+import com.yeoreodigm.server.dto.member.MemberJoinRequestDto;
+import com.yeoreodigm.server.service.EmailService;
+import com.yeoreodigm.server.service.MemberService;
+import com.yeoreodigm.server.service.SurveyService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class MemberApiControllerTest {
+
+    @Mock
+    private MemberService memberService;
+
+    @Mock
+    private SurveyService surveyService;
+
+    @Mock
+    private EmailService emailService;
+
+    @InjectMocks
+    private MemberApiController memberApiController;
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    private MemberJoinRequestDto memberJoinRequestDto;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        memberApiController = new MemberApiController(memberService, surveyService, emailService);
+        mockMvc = MockMvcBuilders.standaloneSetup(memberApiController).build();
+        objectMapper = new ObjectMapper();
+
+        memberJoinRequestDto = new MemberJoinRequestDto();
+        memberJoinRequestDto.setGender(Gender.male);
+        memberJoinRequestDto.setYear(2000);
+        memberJoinRequestDto.setMonth(1);
+        memberJoinRequestDto.setDay(1);
+        memberJoinRequestDto.setRegion("경기");
+        memberJoinRequestDto.setOptional(true);
+    }
+
+    @Test
+    public void 회원가입_테스트() {
+        memberJoinRequestDto.setEmail("kkc217123@naver.com");
+        memberJoinRequestDto.setNickname("testJoin");
+        memberJoinRequestDto.setPassword("qwer1234!");
+        memberApiController.join(memberJoinRequestDto);
+
+        verify(memberService).join(memberJoinRequestDto);
+    }
+
+    @Test
+    public void 회원가입_파라미터누락_테스트() throws Exception {
+        memberJoinRequestDto.setNickname("testJoin");
+        memberJoinRequestDto.setPassword("qwer1234!");
+
+        mockMvc.perform(post("/api/member/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(memberJoinRequestDto)))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+}
---
+++ src/test/java/com/yeoreodigm/server/service/MemberServiceTest.java
@@ -0,0 +1,90 @@
+package com.yeoreodigm.server.service;
+
+import com.yeoreodigm.server.domain.Gender;
+import com.yeoreodigm.server.domain.Member;
+import com.yeoreodigm.server.dto.member.MemberJoinRequestDto;
+import com.yeoreodigm.server.exception.BadRequestException;
+import com.yeoreodigm.server.repository.MemberRepository;
+import com.yeoreodigm.server.repository.SurveyRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
+@Transactional
+public class MemberServiceTest {
+
+    private MemberRepository mockMemberRepository;
+
+    @Mock
+    private SurveyRepository surveyRepository;
+
+    @Mock
+    private TravelNoteService travelNoteService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private AwsS3Service awsS3Service;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Before
+    public void setUp() {
+        Member admin = new Member(
+                "yeoreodigm@naver.com",
+                "qwer1234!",
+                "여러다임",
+                LocalDate.now(),
+                Gender.male,
+                "경기",
+                true);
+
+        mockMemberRepository = mock(MemberRepository.class);
+        when(mockMemberRepository.findByEmail("yeoreodigm@naver.com")).thenReturn(admin);
+        when(mockMemberRepository.findByNickname("여러다임")).thenReturn(admin);
+
+        MockitoAnnotations.openMocks(this);
+        memberService = new MemberService(
+                mockMemberRepository,
+                surveyRepository,
+                travelNoteService,
+                passwordEncoder,
+                emailService,
+                awsS3Service);
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void 회원가입_이메일중복_테스트() {
+        MemberJoinRequestDto memberJoinRequestDto = new MemberJoinRequestDto();
+        memberJoinRequestDto.setEmail("yeoreodigm@naver.com");
+        memberJoinRequestDto.setNickname("여러다임2");
+        memberJoinRequestDto.setPassword("qwer1234!");
+        memberJoinRequestDto.setGender(Gender.male);
+        memberJoinRequestDto.setYear(2000);
+        memberJoinRequestDto.setMonth(1);
+        memberJoinRequestDto.setDay(1);
+        memberJoinRequestDto.setRegion("경기");
+        memberJoinRequestDto.setOptional(true);
+
+        memberService.join(memberJoinRequestDto);
+    }
+
+}
