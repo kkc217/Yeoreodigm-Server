c0df6ce42830aaad0044749b936dcb7c4a40f122
kkc217 <kkc217@naver.com>
2022-09-30 17:06:28 +0900

    음식점 숙소 enum 수정

--- src/main/java/com/yeoreodigm/server/domain/AccommodationType.java
+++ src/main/java/com/yeoreodigm/server/domain/AccommodationType.java
@@ -1,13 +1,13 @@
 package com.yeoreodigm.server.domain;
 
 public enum AccommodationType {
-    OTHERS("기타", 1),
-    PENSION("펜션", 2),
-    MINBAK("민박", 3),
-    MOTEL("모텔", 4),
-    GUEST_HOUSE("게스트하우스", 5),
-    HOTEL("호텔/콘도", 6),
-    CAMPING("캠핑", 7);
+    PENSION("펜션", 1),
+    MINBAK("민박", 2),
+    MOTEL("모텔", 3),
+    GUEST_HOUSE("게스트하우스", 4),
+    HOTEL("호텔/콘도", 5),
+    CAMPING("캠핑", 6),
+    OTHERS("기타", 7);
 
     private final String kName;
 
@@ -22,13 +22,13 @@ public enum AccommodationType {
 
     public static AccommodationType getEnum(int index) {
         return switch (index) {
-            case 0 -> OTHERS;
             case 1 -> PENSION;
             case 2 -> MINBAK;
             case 3 -> MOTEL;
             case 4 -> GUEST_HOUSE;
             case 5 -> HOTEL;
             case 6 -> CAMPING;
+            case 7 -> OTHERS;
             default -> null;
         };
     }
--- src/main/java/com/yeoreodigm/server/domain/RestaurantType.java
+++ src/main/java/com/yeoreodigm/server/domain/RestaurantType.java
@@ -1,13 +1,13 @@
 package com.yeoreodigm.server.domain;
 
 public enum RestaurantType {
-    CLOSURE("폐업", 1),
-    WESTERN("양식", 2),
-    CHINESE("중식", 3),
-    BUNSIK("분식", 4),
-    CAFE("카페/베이커리", 5),
-    KOREAN("한식", 6),
-    JAPANESE("일식", 7);
+    WESTERN("양식", 1),
+    CHINESE("중식", 2),
+    BUNSIK("분식", 3),
+    CAFE("카페/베이커리", 4),
+    KOREAN("한식", 5),
+    JAPANESE("일식", 6),
+    CLOSURE("폐업", 7);
 
     private final String kName;
 
@@ -22,13 +22,13 @@ public enum RestaurantType {
 
     public static RestaurantType getEnum(int index) {
         return switch (index) {
-            case 0 -> CLOSURE;
             case 1 -> WESTERN;
             case 2 -> CHINESE;
             case 3 -> BUNSIK;
             case 4 -> CAFE;
             case 5 -> KOREAN;
             case 6 -> JAPANESE;
+            case 7 -> CLOSURE;
             default -> null;
         };
     }
