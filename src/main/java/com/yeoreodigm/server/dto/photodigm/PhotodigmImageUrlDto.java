ab9b3526b038812afc1d21194c6bea6e896c1cd3
kkc217 <kkc217@naver.com>
2022-10-03 18:24:49 +0900

    포토다임 사진 정보 dto 수정

--- src/main/java/com/yeoreodigm/server/dto/photodigm/PhotodigmImageUrlDto.java
+++ src/main/java/com/yeoreodigm/server/dto/photodigm/PhotodigmImageUrlDto.java
@@ -5,6 +5,7 @@ import com.yeoreodigm.server.domain.Picture;
 import com.yeoreodigm.server.dto.constraint.AWSConst;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.yeoreodigm.server.dto.constraint.AWSConst.*;
@@ -12,22 +13,21 @@ import static com.yeoreodigm.server.dto.constraint.AWSConst.*;
 @Data
 public class PhotodigmImageUrlDto {
 
+    private Long photodigmId;
+
     private String photodigmUrl;
 
-    private String picture1Url;
+    private List<Long> pictureIds = new ArrayList<>();
 
-    private String picture2Url;
-
-    private String picture3Url;
-
-    private String picture4Url;
+    private List<String> pictureUrls = new ArrayList<>();
 
     public PhotodigmImageUrlDto(Photodigm photodigm, List<Picture> pictureList) {
+        this.photodigmId = photodigm.getId();
         this.photodigmUrl = AWS_S3_BASE_URL + AWS_S3_PHOTODIGM_URI + "/" + photodigm.getAddress();
-        this.picture1Url = AWS_S3_BASE_URL + AWS_S3_PICTURE_URI + "/" + pictureList.get(0).getAddress();
-        this.picture2Url = AWS_S3_BASE_URL + AWS_S3_PICTURE_URI + "/" + pictureList.get(1).getAddress();
-        this.picture3Url = AWS_S3_BASE_URL + AWS_S3_PICTURE_URI + "/" + pictureList.get(2).getAddress();
-        this.picture4Url = AWS_S3_BASE_URL + AWS_S3_PICTURE_URI + "/" + pictureList.get(3).getAddress();
+        for (Picture picture : pictureList) {
+            this.pictureIds.add(picture.getId());
+            this.pictureUrls.add(AWS_S3_BASE_URL + AWS_S3_PICTURE_URI + "/" + picture.getAddress());
+        }
     }
 
 }
