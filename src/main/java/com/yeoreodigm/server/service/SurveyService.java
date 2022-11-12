00a431e5760f4607e29cdec5f416ef9a64963c96
kkc217 <kkc217@naver.com>
2022-11-12 20:49:53 +0900

    설문 결과 api 캐싱 수정

--- src/main/java/com/yeoreodigm/server/service/SurveyService.java
+++ src/main/java/com/yeoreodigm/server/service/SurveyService.java
@@ -9,6 +9,7 @@ import com.yeoreodigm.server.dto.survey.SurveyItemDto;
 import com.yeoreodigm.server.repository.MemberRepository;
 import com.yeoreodigm.server.repository.SurveyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,6 +35,7 @@ public class SurveyService {
     }
 
     @Transactional
+    @CacheEvict(value = CacheConst.MEMBER, key = "#member.getEmail()", allEntries = true, condition = "#member != null and #progress == 10")
     public void submitSurveyResult(Member member, Long contentId, int progress) {
         SurveyResult surveyResult = surveyRepository.findSurveyResultByMember(member);
         surveyResult.changeProgress(progress + 1);
